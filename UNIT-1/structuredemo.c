d00a54bdf47b95d237a05daa8551ed9cf2537c0c
Mayursinh D Jadeja <mayursinhdjadeja01@gmail.com>
2026-07-17 12:21:49 +0530

    Refactor student structure demo program
    Updated prompts and output formatting for clarity.

--- UNIT-1/structuredemo.c
+++ UNIT-1/structuredemo.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
+// Write a program to demonstrate structure in C.
 
-struct student {
+#include <stdio.h>
+// Structure declaration
+struct student
+{
     int rollno;
     char name[20];
 };
-
-int main() {
+int main()
+{
     struct student s1;
-
-    printf("Enter Roll No: ");
+    printf("Enter Roll Number: ");
     scanf("%d", &s1.rollno);
-
     printf("Enter Name: ");
     scanf("%s", s1.name);
-
-    printf("\nStudent Data:\n");
-    printf("Roll No: %d\n", s1.rollno);
-    printf("Name: %s\n", s1.name);
-
+    printf("\nStudent Details :- ");
+    printf("Roll Number : %d\n", s1.rollno);
+    printf("Name : %s\n", s1.name);
     return 0;
 }
